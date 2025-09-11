7548ab08f34ba131126dbd02158f5af6a1645c5e
raulgarcia99 <rgarciasamayoa@byupathway.edu>
2025-09-10 23:57:56 -0600

    footer

---
+++ scripts/temples.js
@@ -0,0 +1,8 @@
+const currentyear = document.querySelector("#currentyear");
+today = new Date()
+currentyear.innerHTML = `©${today.getFullYear()}`;
+
+const lastModified = document.querySelector("#lastModified");
+modification = new Date(document.lastModified);
+lastModified.innerHTML = `Last modified: ${new Intl.DateTimeFormat("en-US", {dateStyle: "medium"}).format(modification)} -
+${modification.getHours()}:${modification.getMinutes()}`;
