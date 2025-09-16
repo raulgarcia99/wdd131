8644771b3514ac07972da7c7ae0978f415ca6c03
raulgarcia99 <rgarciasamayoa@byupathway.edu>
2025-09-15 21:31:41 -0600

    wind chill calculation

--- scripts/places.js
+++ scripts/places.js
@@ -1,3 +1,14 @@
+const temperature = 27
+const wind_speed = 15
+
+const compute_wind_chill = (t, ws ) =>
+    13.12 + 0.6215 * t - 11.37 * Math.pow(ws, 0.16) +
+    0.3964 * t * Math.pow(ws, 0.16);
+
+wind_chill = document.querySelector("#wind-chill");
+wind_chill.innerHTML =`${compute_wind_chill(temperature, wind_speed).toFixed(1)}°C`;
+
+
 const currentyear = document.querySelector("#currentyear");
 today = new Date()
 currentyear.innerHTML = `©${today.getFullYear()}`;
