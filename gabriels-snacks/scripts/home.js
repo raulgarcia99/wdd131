f298f09024068340690f001ce12955d43f9afe1d
raulgarcia99 <rgarciasamayoa@byupathway.edu>
2025-10-10 22:51:42 -0600

    Adding a mini database

--- gabriels-snacks/scripts/home.js
+++ gabriels-snacks/scripts/home.js
@@ -7,6 +7,105 @@ hambutton.addEventListener("click", () => {
     hambutton.classList.toggle("open");
 });
 
+const products = [
+  {
+    productType: "snack",
+    productName: "Cappy",
+    price: 0.25,
+    content: "70 gr",
+    imageUrl:
+    "images/cappy.webp"
+  },
+  {
+    productType: "beverage",
+    productName: "Cascada Orange",
+    price: 0.35,
+    content: "355 ml",
+    imageUrl:
+    "images/cascada-orange.webp"
+  },
+  {
+    productType: "snack",
+    productName: "Club Extra",
+    price: 0.40,
+    content: "225 gr",
+    imageUrl:
+    "images/club-extra.webp"
+  },
+  {
+    productType: "beverage",
+    productName: "Coca-Cola",
+    price: 0.85,
+    content: "355 ml",
+    imageUrl:
+    "images/coca-cola.webp"
+  },
+  {
+    productType: "beverage",
+    productName: "Del Valle Tangerine",
+    price: 0.60,
+    content: "500 ml",
+    imageUrl:
+    "images/del-valle-tangerine.webp"
+  },
+  {
+    productType: "snack",
+    productName: "Lays Classic",
+    price: 1.00,
+    content: "42.5 gr",
+    imageUrl:
+    "images/coca-cola.webp"
+  },
+  {
+    productType: "beverage",
+    productName: "Salutaris Lemon",
+    price: 0.75,
+    content: "355 ml",
+    imageUrl:
+    "images/salutaris-lemon.webp"
+  },
+  {
+    productType: "beverage",
+    productName: "7UP",
+    price: 0.75,
+    content: "355 ml",
+    imageUrl:
+    "images/seven-up.webp"
+  },
+  {
+    productType: "snack",
+    productName: "Tosh Coconut",
+    price: 0.85,
+    content: "355 ml",
+    imageUrl:
+    "images/coca-cola.webp"
+  },
+  {
+    productType: "snack",
+    productName: "Toztecas",
+    price: 0.25,
+    content: "130 gr",
+    imageUrl:
+    "images/toztecas.webp"
+  },
+  {
+    productType: "beverage",
+    productName: "V8 Tropical Blend",
+    price: 1.25,
+    content: "473 ml",
+    imageUrl:
+    "images/v8-tropical-blend.webp"
+  },
+  {
+    productType: "snack",
+    productName: "Yummi Pops",
+    price: 0.40,
+    content: "120 gr",
+    imageUrl:
+    "images/yummi-pops.webp"
+  }  
+]
+
 // Footer
 
 const currentyear = document.querySelector("#currentyear");
